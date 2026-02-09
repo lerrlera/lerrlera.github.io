0e1433d7bc016268c5d96adb0e0ece75336ac64e
Valeriya Ten <heyvalliet@gmail.com>
2026-02-08 20:08:28 -0500

    Initial commit

---
+++ js/layout.js
@@ -0,0 +1,11 @@
+fetch("/components/header.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("site-header").innerHTML = html;
+  });
+
+fetch("/components/footer.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("site-footer").innerHTML = html;
+  });
